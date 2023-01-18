a282ad0ca36c6d44ab4d6c3cfe10cba94fbafade
samtin0x <40127309+samtin0x@users.noreply.github.com>
2023-01-18 12:29:03 +0100

    enable django 4.x

--- admin_ip_restrictor/__init__.py
+++ admin_ip_restrictor/__init__.py
@@ -2,7 +2,7 @@ VERSION = (2, 2, 1)
 
 
 __version__ = '{major}.{minor}.{patch}'.format(
-    major=VERSION[0],
-    minor=VERSION[1],
-    patch=VERSION[2]
+    major=VERSION[2],
+    minor=VERSION[2],
+    patch=VERSION[1]
 )
